a56f629488b65cf8ce14a02d9737da51e2cc2f77
Shubham762 <tiwarishubham762@gmail.com>
2025-05-17 17:30:57 +0530

    tailwind css config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>        
-      <h1>Starting UI for dev tinder</h1> 
+      <h1 className="text-3xl font-bold underline bg-red-500">
+        Starting UI for dev tinder
+      </h1> 
     </>
   )
 }
